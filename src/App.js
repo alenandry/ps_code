ccd71eabe3b9e0f2ea098d443991e82d10119f63
alenandry <45224597+alenandry@users.noreply.github.com>
2021-04-10 13:34:13 +0800

    chnge

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ export default class App extends Component {
     p5.frameRate(this.fr);
     // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
   };
-  draw = p5 => {
-    p5.fill(234, 31, 81);
+  draw = (p5) => {
+    p5.fill(234, 31, 0);
     p5.noStroke();
-    p5.rect(50, 50, 250, 250);
-    p5.fill(255);
+    p5.rect(50, 50, 350, 250);
+    p5.fill(25);
     p5.textStyle(p5.BOLD);
-    p5.textSize(140);
-    p5.text("p5*", 60, 250);
+    p5.textSize(150);
+    p5.text("Alen", 60, 250);
   };
 
   render() {
